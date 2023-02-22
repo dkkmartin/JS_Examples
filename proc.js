db61b8d7bce66b5eb89122d009816c135f61aa91
martin <martinbruun1995@gmail.com>
2023-02-22 13:12:28 +0100

    Final final

--- proc.js
+++ proc.js
@@ -7,7 +7,7 @@ const bigImg = document.querySelector('.bigImg')
 const thumbnails = document.querySelectorAll('.thumbnails img')
 
 // Bruges til at sætte tal på, på det billede vi skal bruge med arrows
-let index = 1;
+let index = 0
 
 // Lytter efter 'click' på de elementer vi har taget fat i 
 //Når der bliver klikket sender den info fra hvilket thumbnail der blev klikket med
@@ -18,51 +18,42 @@ let index = 1;
 for (let i = 0; i < thumbnails.length; i++) {
     thumbnails[i].addEventListener('click', () => {
         changePhotoClick(thumbnails[i])
-        setActive(thumbnails[i])
+        index = i
+        setActive(index)
     })
 }
 
-//Lytter efter clicks på arrows og køre function der tilhøre dem
-right.addEventListener('click', showNext)
-left.addEventListener('click', showPrev)
+//Lytter efter clicks på arrows og køre functioner
+right.addEventListener('click', () => {
+    changePhotoArrowRight()
+})
 
-//function for når left arrow bliver klikket
-function showPrev() {
-    //hvis index er større end 1
-    if (index > 1) {
-        //index skal minus 1 fra sig selv
-        index--; 
-    }
-
-    //Hvis ikke index er større end 1
-    else {
-        index = 6;
-    }
-    changePhotoArrow();
-}
-
-//function for når right arrow bliver klikket
-function showNext() {
-
-    //hvis index er mindre end 1
-    if (index < 6) {
-
-        //index skal plus 1 på sig selv
-        index++; 
-    }
-
-    //Hvis ikke index er mindre end 6
-    else {
-        index = 1;
-    }
-    changePhotoArrow();
-}
+left.addEventListener('click', () => {
+    changePhotoArrowLeft()
+})
 
 //function for at skifte src på bigImg med brug af vores index variable -
 //og som bliver kørt når vi trykker på arrows
-function changePhotoArrow() {
-    let newSrc = 'images/proc_' + index + '.jpg'
+function changePhotoArrowRight() {
+    if (index >= 5){
+        index = 0
+    } else {
+        index++
+    }
+    let newSrc = thumbnails[index].getAttribute('src')
     bigImg.setAttribute('src', newSrc)
+    setActive(index)
+}
+
+function changePhotoArrowLeft() {
+    if (index === 0){
+        index = 5
+    } else {
+        index--
+    }
+    let newSrc = thumbnails[index].getAttribute('src')
+    bigImg.setAttribute('src', newSrc)
+    setActive(index)
 }
 
 //function når thumbnails bliver klikket.
@@ -77,19 +68,21 @@ function changePhotoClick(e) {
 
 //function for at sætte border på de valgte billeder
 //og fjerne border på sidste aktive billed
-function setActive(e) {
+function setActive(dataNumber) {
+    //Gemmer det nuværende aktive billede in en variable
+    let currentActive = document.querySelector(`[data-number="${dataNumber}"]`)
 
-    //Gemmer det aktive billede
+    //Gemmer det aktive billede i en variable
     let lastActive = document.querySelector('.active')
 
     //Hvis det klikkede billede (e) 
     //ikke er lige med det sidste aktive billede (lastActive)
-    if (e !== lastActive) {
+    if (dataNumber !== lastActive) {
 
         //Sidste aktive får fjernet sin border
         lastActive.classList.remove('active')
 
         //Den nye klikkede får en border
-        e.classList.add('active')
+        currentActive.classList.add('active')
     } 
 }
